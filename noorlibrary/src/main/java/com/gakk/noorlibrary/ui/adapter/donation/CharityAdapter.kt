21b3066890f3a93eb573cc0b3293e45b7d4c0494
Mehnaz <mehnaz.gakk@gmail.com>
2023-01-24 16:39:26 +0600

    Donation and eid jamat binding remove

--- noorlibrary/src/main/java/com/gakk/noorlibrary/ui/adapter/donation/CharityAdapter.kt
+++ noorlibrary/src/main/java/com/gakk/noorlibrary/ui/adapter/donation/CharityAdapter.kt
@@ -1,13 +1,22 @@
 package com.gakk.noorlibrary.ui.adapter.donation
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
+import android.view.View.GONE
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.appcompat.widget.AppCompatTextView
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.gakk.noorlibrary.R
 import com.gakk.noorlibrary.callbacks.DetailsCallBack
 import com.gakk.noorlibrary.databinding.LayoutItemCharityOrganizationsBinding
 import com.gakk.noorlibrary.model.literature.Literature
+import com.gakk.noorlibrary.ui.adapter.ZAKAT_CALC_VIEW
+import com.gakk.noorlibrary.ui.adapter.ZakatListAdapter
 import com.gakk.noorlibrary.util.FragmentProvider
 import com.gakk.noorlibrary.util.ORGANIZATION_DETAILS
 import com.gakk.noorlibrary.util.handleClickEvent
@@ -18,26 +27,30 @@ internal class CharityAdapter(
 ) :
     RecyclerView.Adapter<CharityAdapter.ViewHolder>() {
 
-    inner class ViewHolder(binding: LayoutItemCharityOrganizationsBinding) :
-        RecyclerView.ViewHolder(binding.root) {
-        var charityBinding: LayoutItemCharityOrganizationsBinding? = binding
+    inner class ViewHolder(layoutView: View) :
+        RecyclerView.ViewHolder(layoutView) {
+        var view: View = layoutView
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding: LayoutItemCharityOrganizationsBinding= DataBindingUtil.inflate(
-            LayoutInflater.from(parent.context),
-            R.layout.layout_item_charity_organizations,
-            parent,
-            false
-        )
-        return ViewHolder(binding)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):ViewHolder {
+        val  view = LayoutInflater.from(parent.context).inflate(R.layout.layout_item_charity_organizations,parent,false)
+
+        return ViewHolder(view)
     }
+
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val listItem = literatureList.get(position)
-        holder.charityBinding?.literature = listItem
-
-        holder.charityBinding?.clItem?.handleClickEvent {
+      //  holder.charityBinding?.literature = listItem
+       val tvTitle = holder.itemView.findViewById<AppCompatTextView>(R.id.textViewNormal9)
+        tvTitle.text = listItem.subcategoryName
+        val tvText = holder.itemView.findViewById<AppCompatTextView>(R.id.textViewNormal20)
+        tvText.text = listItem.textInArabic
+        val image = holder.itemView.findViewById<AppCompatImageView>(R.id.appCompatImageView10)
+        Glide.with(holder.view.context).load(listItem.fullImageUrl).into(image)
+        val progressBar = holder.view.findViewById<ProgressBar>(R.id.progressBar)
+             progressBar.visibility = GONE
+        holder.itemView?.handleClickEvent {
             val fragment = FragmentProvider.getFragmentByName(
                 ORGANIZATION_DETAILS,
                 detailsActivityCallBack = detailsCallBack,
--- noorlibrary/src/main/java/com/gakk/noorlibrary/ui/adapter/donation/DonationAdapter.kt
+++ noorlibrary/src/main/java/com/gakk/noorlibrary/ui/adapter/donation/DonationAdapter.kt
@@ -1,9 +1,15 @@
 package com.gakk.noorlibrary.ui.adapter.donation
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.cardview.widget.CardView
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.gakk.noorlibrary.R
 import com.gakk.noorlibrary.callbacks.DetailsCallBack
 import com.gakk.noorlibrary.databinding.LayoutItemDonationBinding
@@ -18,27 +24,35 @@ internal class DonationAdapter(
 ) :
     RecyclerView.Adapter<DonationAdapter.ViewHolder>() {
 
-    inner class ViewHolder(binding: LayoutItemDonationBinding) :
-        RecyclerView.ViewHolder(binding.root) {
-        var donationBinding: LayoutItemDonationBinding? = binding
+    inner class ViewHolder(layoutView: View) :
+        RecyclerView.ViewHolder(layoutView) {
+       // var donationBinding: LayoutItemDonationBinding? = binding
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding: LayoutItemDonationBinding = DataBindingUtil.inflate(
-            LayoutInflater.from(parent.context),
-            R.layout.layout_item_donation,
-            parent,
-            false
-        )
-        return ViewHolder(binding)
+        val  view = LayoutInflater.from(parent.context).inflate(R.layout.layout_item_donation,parent,false)
+
+        return ViewHolder(view)
+//        val binding: LayoutItemDonationBinding = DataBindingUtil.inflate(
+//            LayoutInflater.from(parent.context),
+//            R.layout.layout_item_donation,
+//            parent,
+//            false
+//        )
+//        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val listItem = literatureList.get(position)
-
-        holder.donationBinding?.literature = listItem
-
-        holder.donationBinding?.cardItem?.handleClickEvent {
+        val image = holder.itemView.findViewById<AppCompatImageView>(R.id.appCompatImageView12)
+        Glide.with( holder.itemView.context).load(listItem.fullImageUrl).into(image)
+        val tvTitle = holder.itemView.findViewById<AppCompatTextView>(R.id.tvTitle)
+        tvTitle.text = listItem.subcategoryName
+        val progressBar = holder.itemView.findViewById<ProgressBar>(R.id.progressBar)
+        progressBar.visibility = View.GONE
+       // holder.donationBinding?.literature = listItem
+       val cardItem = holder.itemView.findViewById<CardView>(R.id.cardItem)
+            cardItem?.handleClickEvent {
             val fragment = FragmentProvider.getFragmentByName(
                 PAGE_DONATE_ZAKAT,
                 detailsActivityCallBack = detailsCallBack,
